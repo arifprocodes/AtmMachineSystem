b6adad40fa2d67911baa8beda9215271710897a2
Practice432 <ariful168bd@gmail.com>
2023-05-11 14:20:24 -0400

    Validation

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,38 @@ public class Main {
         int accountNumber=123;
         int accuntPin=123;
         Scanner in = new Scanner(System.in);
-        System.out.print("Welcome to ATM Machine ");
+        System.out.println("Welcome to ATM Machine ");
         System.out.print("Enter Account Number : ");
         int UserAccNumber = in.nextInt();
         System.out.print("Enter Pin : ");
         int UserPin = in.nextInt();
         if ((UserAccNumber==accountNumber)&&(UserPin==accuntPin)){
             while(true){
-                System.out.print("1.View Available Balance\n2.Withdraw Amount")
+                System.out.println("1.View Available Balance\n2.Withdraw Amount\n3.Diposit Amount\n4.View Statement\n5.Exit");
+                System.out.println("Enter choice : ");
+                int ch=in.nextInt();
+                if(ch==1){
+                    
+                } else if (ch==2) {
+                    
+                } else if (ch==3) {
+                    
+                } else if (ch==4) {
+                    
+                } else if (ch==5) {
+                    System.out.println("Collect your card\nThank you for using ATM Machine");
+                    System.exit(0);
+                    
+                }else {
+                    System.out.println("Enter Correct choice");
+                }
+
             }
         }else {
-            System.out.println("Incorrent Account or Pin ");
+            System.out.println("Incorrect Account or Pin ");
             System.exit(0);
         }
-
+        
 
     }
 }
